fe2c8c8434b7f7a0518b4fbf48c302945035ccec
steveneross94 <ross.steven@me.com>
2020-06-07 14:24:45 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Song.destroy_all
+Artist.destroy_all
+Genre.destroy_all
+# g1 = Genre.create(name: "Pop")
+# g2 = Genre.create(name: "Rap")
+# g3 = Genre.create(name: "Punk")
+# g4 = Genre.create(name: "Rock")
+# g5 = Genre.create(name: "Blues")
+
+# art1 = Artist.create(name: "B.B. King", bio: "test bio")
+# art2 = Artist.create(name: "Lady Gaga", bio: "test bio")
+
+
+
+# Song.create(name: "Thrill is Gone", artist_id: art1.id, genre_id: g5.id)
