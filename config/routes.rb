6711152b4762bc59bf19321f251012a7ab7c866a
CMSTie <cmstie@gmail.com>
2020-06-08 12:00:51 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :songs, only: [:edit, :index, :new, :create, :update, :show, :destory]
+
+  resources :artists, only: [:edit, :index, :new, :create, :update, :show, :destory]
+
+  resources :genres, only: [:edit, :index, :new, :create, :update, :show, :destory]
 end
