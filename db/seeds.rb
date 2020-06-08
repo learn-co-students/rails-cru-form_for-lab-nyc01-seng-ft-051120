150833a24a248341f78bfd27a783df54a23796aa
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2020-06-08 13:24:15 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Song.destroy_all
+Artist.destroy_all
+Genre.destroy_all
+kanye = Artist.create(name: "Ye", bio:"Can't tell me Nothin'!")
+hiphop = Genre.create(name: "hip-hop")
+beyonce = Artist.create(name: "Beyonce", bio:"Hips TikTok when I dance")
+rnb = Genre.create(name: "r & b")
+mac_miller = Artist.create(name: "Mac Miller", bio:"Circles")
+alt = Genre.create(name: "alternative r & b")
+Song.create(name: "Homecoming", artist_id: kanye.id, genre_id: hiphop.id)
+Song.create(name: "Savage remix", artist_id: beyonce.id, genre_id: rnb.id)
+Song.create(name: "Dunno", artist_id: mac_miller.id, genre_id: alt.id)
